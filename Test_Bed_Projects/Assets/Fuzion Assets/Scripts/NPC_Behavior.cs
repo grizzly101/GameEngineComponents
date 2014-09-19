5e213925ac1506224dabf63ac8a1bfe1a66d3c63
grizzly101 <blackford.jay@gmail.com>
2014-09-18 20:34:33 -0400

    Created simple mecanim state machine for frog.  Allows transistions to jump_forward from idle and blends a separate layer for playing the talking animation of the idle animation.

--- Test_Bed_Projects/Assets/Fuzion Assets/Scripts/NPC_Behavior.cs
+++ Test_Bed_Projects/Assets/Fuzion Assets/Scripts/NPC_Behavior.cs
@@ -3,6 +3,9 @@ using System.Collections;
 using System.Collections.Generic;
 public class NPC_Behavior : MonoBehaviour {
 
+	private Animator anim;
+	private AnimatorStateInfo crntBaseState;
+	private AnimatorStateInfo layer2CrntState;
 	public DialogueStateMachine dial_manager;
 	public AudioClip	m_voice;
 	public AudioSource  m_mouth;
@@ -14,12 +17,39 @@ public class NPC_Behavior : MonoBehaviour {
 
    	void Start()
 	{
-
+		anim = GetComponent<Animator>();
+		if(anim.layerCount == 2)
+		{
+			anim.SetLayerWeight(1,1);
+		}
 		m_mouth = this.gameObject.AddComponent<AudioSource>();
 		m_mouth.playOnAwake = false;
 
 		//player_dialogue_options.Add (new string ("Ask about the key"));
 		//player_dialogue_options.Add (new string ("Exit Conversation"));
+
+	}
+
+	void FixedUpdate()
+	{
+		crntBaseState = anim.GetCurrentAnimatorStateInfo (1);
+
+		if(anim.layerCount == 2)
+		{
+			layer2CrntState = anim.GetCurrentAnimatorStateInfo(1);
+		}
+
+		if(Input.GetKey("up"))
+		{
+
+			anim.SetBool ("talk_now",true);
+			print ("up");
+		}
+
+	}
+
+	void OnAnimatorMove()
+	{
 
 	}
 
